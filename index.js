89da9b936772fcbd55e8f09a0bbf1c2f2dcd6089
Junjie <jzhang94@horizon.csueastbay.edu>
2019-10-01 00:18:09 -0700

    commit for deploy to heroku1

--- index.js
+++ index.js
@@ -37,3 +37,43 @@ showTimes = () => {
     }
     return result;
 }
+
+//PUT INSIDE YOUR index.js file (or whereever)
+// Assumes DATABASE_URL in the heroku configs points to your PostGres database
+// setups the url /db to connect to database and does a query from a table test_table
+var pg = require('pg');
+app.get('/db', function (request, response) {
+    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+        client.query('SELECT * FROM test_table', function(err, result) {
+            done();
+            if (err)
+            { console.error(err); response.send("Error " + err); }
+            else
+            { response.render('pages/db', {results: result.rows} ); }
+        });
+    });
+});
+
+//**************************************************************************
+//***** mongodb get all of the Routes in Routes collection where frequence>=1
+//      and sort by the name of the route.  Render information in the views/pages/mongodb.ejs
+app.get('/mongodb', function (request, response) {
+
+    mongodb.MongoClient.connect(process.env.MONGODB_URI, function(err, db) {
+        if(err) throw err;
+        //get collection of routes
+        var Routes = db.collection('Routes');
+        //get all Routes with frequency >=1
+        Routes.find({ frequency : { $gte: 0 } }).sort({ name: 1 }).toArray(function (err, docs) {
+            if(err) throw err;
+
+            response.render('pages/mongodb', {results: docs});
+
+        });
+
+        //close connection when your app is terminating.
+        db.close(function (err) {
+            if(err) throw err;
+        });
+    });//end of connect
+});//end app.get
