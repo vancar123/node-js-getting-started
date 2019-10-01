fe375114543622e8eb2a85386f0c1f7797e29584
Junjie <jzhang94@horizon.csueastbay.edu>
2019-10-01 00:29:11 -0700

    Add cool face API

--- index.js
+++ index.js
@@ -38,19 +38,3 @@ showTimes = () => {
     return result;
 }
 
-//PUT INSIDE YOUR index.js file (or whereever)
-// Assumes DATABASE_URL in the heroku configs points to your PostGres database
-// setups the url /db to connect to database and does a query from a table test_table
-var pg = require('pg');
-app.get('/db', function (request, response) {
-    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-        client.query('SELECT * FROM test_table', function(err, result) {
-            done();
-            if (err)
-            { console.error(err); response.send("Error " + err); }
-            else
-            { response.render('pages/db', {results: result.rows} ); }
-        });
-    });
-});
-
