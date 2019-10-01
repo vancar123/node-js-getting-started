05ec4d8edce8c69e7955feb11245eb7f4fac001f
Junjie <jzhang94@horizon.csueastbay.edu>
2019-10-01 00:21:23 -0700

    Add cool face API

--- index.js
+++ index.js
@@ -54,26 +54,3 @@ app.get('/db', function (request, response) {
     });
 });
 
-//**************************************************************************
-//***** mongodb get all of the Routes in Routes collection where frequence>=1
-//      and sort by the name of the route.  Render information in the views/pages/mongodb.ejs
-app.get('/mongodb', function (request, response) {
-
-    mongodb.MongoClient.connect(process.env.MONGODB_URI, function(err, db) {
-        if(err) throw err;
-        //get collection of routes
-        var Routes = db.collection('Routes');
-        //get all Routes with frequency >=1
-        Routes.find({ frequency : { $gte: 0 } }).sort({ name: 1 }).toArray(function (err, docs) {
-            if(err) throw err;
-
-            response.render('pages/mongodb', {results: docs});
-
-        });
-
-        //close connection when your app is terminating.
-        db.close(function (err) {
-            if(err) throw err;
-        });
-    });//end of connect
-});//end app.get
