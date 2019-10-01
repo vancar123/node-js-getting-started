8ef5f5df2e062b5ef0a47eee357941ab0544b356
Junjie <jzhang94@horizon.csueastbay.edu>
2019-10-01 01:24:53 -0700

    Add cool face API3

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const cool = require('cool-ascii-faces')
 const express = require('express')
 const path = require('path')
-const PORT = process.env.PORT || const { Pool } = require('pg');
+const PORT = process.env.PORT || 5000
+const { Pool } = require('pg');
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: true
 });
+
 
 express()
     .use(express.static(path.join(__dirname, 'public')))
